2ad3b807914ddc89b24a652f8eecfefce183ecf3
kimgeonil <kji8922@naver.com>
2024-01-29 22:10:58 +0900

    feat: project init & develop setting

---
+++ .eslintrc.js
@@ -0,0 +1,91 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'import',
+    'unused-imports',
+    '@tanstack/query',
+    'prettier',
+  ],
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'airbnb',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-param-reassign': ['error', { props: false }],
+    'unused-imports/no-unused-imports': 'error',
+    'react/jsx-props-no-spreading': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.tsx', '.ts'] }],
+    'react/react-in-jsx-scope': 'off',
+    'react/no-array-index-key': 'warn',
+    'react/require-default-props': 'off',
+    'react/jsx-no-useless-fragment': 'off',
+    'react-hooks/exhaustive-deps': [
+      'error',
+      {
+        enableDangerousAutofixThisMayCauseInfiniteLoops: true,
+      },
+    ],
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-named-as-default': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-cycle': 'off',
+    'import/extensions': [
+      'error',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        json: 'ignorePackages',
+      },
+    ],
+    'jsx-a11y/click-events-have-key-events': 'off',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+    'jsx-a11y/no-static-element-interactions': 'off',
+  },
+  overrides: [
+    {
+      files: '**/*.+(ts|tsx)',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint'],
+      extends: ['airbnb-typescript', 'plugin:@typescript-eslint/recommended'],
+      rules: {
+        '@typescript-eslint/quotes': [
+          2,
+          'backtick',
+          {
+            avoidEscape: true,
+          },
+        ],
+        '@typescript-eslint/space-before-blocks': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/comma-dangle': 'off',
+        '@typescript-eslint/indent': 'off',
+      },
+    },
+  ],
+};}
